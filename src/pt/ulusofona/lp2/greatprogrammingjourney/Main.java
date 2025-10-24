934cc3fcc20ed57d3c26f12f795b667cdfd553c8
Filipe Afonso <filipeandre1710@gmail.com>
2025-10-24 17:25:32 +0100

    AUTHORS.txt

--- src/pt/ulusofona/lp2/greatprogrammingjourney/Main.java
+++ src/pt/ulusofona/lp2/greatprogrammingjourney/Main.java
@@ -27,7 +27,7 @@ public class Main {
         novo.listaJogadores.put(teste.getId(),teste);
         novo.listaJogadores.put(teste2.getId(),teste2);
         System.out.println(Arrays.toString(novo.getSlotInfo(5)));
-        //teste para o github aaaaaaaaaaaaa
+        //teste para o github
 
 
     }
